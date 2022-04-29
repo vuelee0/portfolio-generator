921139cce212173a09b6aab2b0796d4a904d6765
Vue Lee <vuelee09@gmail.com>
2022-04-29 00:57:27 -0500

    add when method

--- app.js
+++ app.js
@@ -32,9 +32,22 @@ const promptUser = () => {
             message: 'Enter your GitHub Username'
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
