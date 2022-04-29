6a579bf6ab65f315ca879b0b69e5306f336557f6
Vue Lee <vuelee09@gmail.com>
2022-04-28 19:18:58 -0500

    install inquirer

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1){
-        console.log(profileDataArr[i]);
-    }
+// const pageHTML = generatePage(name, github);
 
-    console.log('================');
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-printProfileData(profileDataArgs);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1){
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
